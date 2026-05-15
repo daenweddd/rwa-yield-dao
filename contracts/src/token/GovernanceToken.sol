e6103f651c7d94d1c75a727d2b3ce2c2410d0c49
maratovaoka-png <maratovaoka@gmail.com>
2026-05-15 17:27:12 +0500

    feat(token): add governance token

--- contracts/src/token/GovernanceToken.sol
+++ contracts/src/token/GovernanceToken.sol
@@ -1,2 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor(address initialReceiver)
+        ERC20("RealYield Governance Token", "RYG")
+        ERC20Permit("RealYield Governance Token")
+    {
+        require(initialReceiver != address(0), "zero receiver");
+        _mint(initialReceiver, 1_000_000 ether);
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
